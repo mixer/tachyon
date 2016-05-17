0ab731349e91df58575cc6523e77bfd81b264a32
sslivins <sslivins@gmail.com>
2016-05-16 18:38:34 -0700

    copy libcef to path that plugin is looking for it

--- build_scripts/build_tachyon_windows_vs.bat
+++ build_scripts/build_tachyon_windows_vs.bat
@@ -4,22 +4,25 @@ SET FFmpegPath64=C:\beam\dependencies2015\win64
 SET x264Path64=C:\beam\dependencies2015\win64
 SET curlPath64=C:\beam\dependencies2015\win64
 SET QTDIR64=C:\Qt\5.6\msvc2015_64
+REM Microsoft Build Tools 2015 https://www.microsoft.com/en-us/download/details.aspx?id=4815
 SET PATH=%PATH%;C:\Program Files (x86)\MSBuild\14.0\Bin
 REM Chromium Embedded Framework https://bitbucket.org/chromiumembedded/cef
 echo "building CEF browser plugin"
 REM call python automate-git.py --download-dir=../.. --branch=2623 --x64-build
+REM SET cef_root_dir=%cd%\cef
 pushd .
 cd ..\..\
 REM windows 64 bit binary: https://cefbuilds.com/#branch_2623
-REM SET cef_root_dir=%cd%\cef
 SET cef_root_dir=%cd%\cef_binary
-REM cd cef_binary
-REM mkdir build
-REM cd build
-REM cmake -G "Visual Studio 14 2015 Win64" ..
-REM msbuild /p:Configuration=Release,Platform=x64 ALL_BUILD.vcxproj
+cd cef_binary
+mkdir build
+cd build
+cmake -G "Visual Studio 14 2015 Win64" ..
+msbuild /p:Configuration=Release,Platform=x64 ALL_BUILD.vcxproj
+REM current CEF plugin expects this one folder down
+copy libcef_dll\Release\libcef_dll_wrapper.lib libcef_dll\
 REM /machine is set incorrectly should be x64
-REM cd..\..
+cd..\..
 echo "building libftl"
 call git clone https://github.com/WatchBeam/ftl-sdk.git
 cd ftl-sdk
