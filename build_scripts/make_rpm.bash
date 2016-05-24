484a311c27abd6f9e0414180b10d7c41589c36ea
Stefan Slivinski <sslivins@gmail.com>
2016-05-24 15:22:36 -0700

    removed dependancies from rpm package

--- build_scripts/make_rpm.bash
+++ build_scripts/make_rpm.bash
@@ -1,8 +1,5 @@
 fpm --rpm-summary "Tachyon OBS-Studio Fork with FTL (faster than light)" \
 	-n tachyon -v 1.1 --prefix /opt/tachyon -C /opt/tachyon -s dir -t rpm -p ../../NAME_VERSION_ARCH.rpm \
-	-d "qt5-qtx11extras >= 5.6.0" \
-	-d "SDL >= 1.2.15" \
-	-d "jack-audio-connection-kit >= 1.9.9" \
 	--after-install rpm_post_install \
 	--after-remove rpm_post_remove
 	
