dc79c0887e39a943029d4d438847d9fccca1cbbc
Stefan Slivinski <sslivins@gmail.com>
2016-05-24 15:19:36 -0700

    added obs version override to linux build script and fixed path of source files to make deb

--- build_scripts/make_deb.bash
+++ build_scripts/make_deb.bash
@@ -1,6 +1,3 @@
-fpm -n tachyon -v 1.0 -C /tmp/tachyon -s dir -t deb -p ../NAME_VERSION_ARCH.deb \
-	-d "qt5-qtx11extras >= 5.6.0" \
-	-d "SDL >= 1.2.15" \
-	-d "jack-audio-connection-kit >= 1.9.9" \
+fpm -n tachyon -v 1.1 --prefix /opt/tachyon -C /opt/tachyon -s dir -t deb -p ../../NAME_VERSION_ARCH.deb \
 	--after-install deb_post_install \
 	--after-remove deb_post_remove
