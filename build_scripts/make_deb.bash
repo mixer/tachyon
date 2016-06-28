327d3b31a61418f23d75337dd8902eb422901ace
Stefan Slivinski <sslivins@gmail.com>
2016-06-28 08:34:02 -0700

    fixed linux script to work with correct path of ftl

--- build_scripts/make_deb.bash
+++ build_scripts/make_deb.bash
@@ -1,3 +1,3 @@
-fpm -n tachyon -v 1.2.6 --prefix /opt/tachyon -C /opt/tachyon -s dir -t deb -p ../../NAME_VERSION_ARCH.deb \
+fpm -n tachyon -v 1.2.7 --prefix /opt/tachyon -C /opt/tachyon -s dir -t deb -p ../../NAME_VERSION_ARCH.deb \
 	--after-install deb_post_install \
 	--after-remove deb_post_remove
